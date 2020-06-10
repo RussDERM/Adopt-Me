1dddb30888ca8dfd9887a3baaa9349bed75e5d83
Russell Dermody <russ.dermody@gmail.com>
2020-06-10 14:52:10 -0700

    API working, returning populated components

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -7,11 +8,43 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  // * Async Function to request Pets
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+    // * set pets state to object {animals}, as defined above
+    setPets(animals || []);
+  }
+
+  useEffect(
+    () => {
+      // * Clears out Breeds
+      setBreeds([]);
+      setBreed("");
+
+      // * Breeds request
+      pet.breeds(animal).then(({ breeds }) => {
+        const breedStrings = breeds.map(({ name }) => name);
+        setBreeds(breedStrings);
+      }, console.error);
+    },
+    /* useEffect dependencies */ [animal, setBreed, setBreeds]
+  );
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -25,6 +58,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
