6b9717e1c515fed72d1ec0e87b6c7fd22951e155
Russell Dermody <russ.dermody@gmail.com>
2020-06-15 10:18:05 -0700

    ReachRouter Implimented!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./details";
 // import Pet from "./Pet";
 
 const App = () => {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Ponyo" animal="Goldfish" breed="Unknown" />
-        <Pet name="Onyx" animal="Cat" breed="Black" />
-        <Pet name="Taven" animal="Dog" breed="Pitbull" /> */}
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
